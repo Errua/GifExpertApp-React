66913586b6853f1a622229c77433f002e2c6efb6
Pablo Ruano <Pabloruano777@gmail.com>
2020-09-07 12:06:18 +0200

    renombramos build por docs

---
+++ docs/precache-manifest.b55dc9c5b9c9fd8e33d3fe1cce13b525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20706b0c77603242ce22ab913b0c0d7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dfe87fa6a1378e470e5",
+    "url": "/static/css/main.6a03cb2e.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dfe87fa6a1378e470e5",
+    "url": "/static/js/main.ac9caa31.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
